734d63ebe73fdd0d384ed91d228daaf125aaba16
renarfreitas <renarfreitas@gmail.com>
2018-12-15 01:19:19 -0300

    Comite inicial das aulas do livro Head First Python

---
+++ vowels2.py
@@ -0,0 +1,10 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = "Milliways"
+found = []
+for letter in word:
+    if letter in vowels:
+        if letter no in found:
+            foun.append(letter)
+for vowel in found:
+    print (vowel)
+
