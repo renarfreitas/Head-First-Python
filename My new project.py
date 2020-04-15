1c3c528dc46c885084420fd4d27dabea92456323
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2020-04-14 22:56:07 -0300

    Commit Course of Python 3
    Module 1 Section 1: Starting Jupyter Notebooks
    
    1.1 Intro Python
    Jupyter Notebook: MOD01_1-1.1_Intro_Python.ipynb
    
    Getting started with Python in Jupyter Notebooks
    This unit will cover
    
    Python 3 in Jupyter notebooks
    print()
    comments
    data types basics

---
+++ My new project.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 3
+
+
+# In[2]:
+
+
+print(x)
+
+
+# In[8]:
+
+
+#Good code is commented code
+#This is "Hello World!" for Python 3
+print('Hello World!')
+
+
+# In[10]:
+
+
+print("Hello students")
+
+
+# # Use Jupyter to
+# .Edit
+# .Run
+# .Save
+
+# In[15]:
+
+
+x = 2
+y = x + 5 + 3
+y
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
