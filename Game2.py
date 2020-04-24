9867d17382194396959025849a3e45b5c7835d2b
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2020-04-23 23:57:25 -0300

    Commit second game2
    Continuation of studies on programming using python

---
+++ Game2.py
@@ -0,0 +1,12 @@
+print("Welcome!")
+g = input("Guess te number: ")
+guess = int(g)
+if guess == 5:
+    print("You win!")
+else:
+    if guess > 5:
+        print("Too high! * Try again...")
+    else:
+        print ("Too low! * Try again...!")
+print("Game over!")
+
